d1fd448e71598dc25c3d8084ce45374fccd2eb1c
jahid1604091 <jh409780@gmail.com>
2023-08-14 15:47:38 +0600

    sdfssdfddf

--- controllers/user.js
+++ controllers/user.js
@@ -17,7 +17,7 @@ export const auth_user = asyncHandler(async (req, res) => {
     res.cookie("jwt", user.getSignedJwtToken(), {
       httpOnly: true,
       secure: process.env.NODE_ENV !== "development",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 30 * 24 * 24 * 60 * 60,
     });
     
