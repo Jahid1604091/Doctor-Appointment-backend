f3ff9f3d5b6ab1533c8e2e4a3e09e26aa3d48ae8
jahid1604091 <jh409780@gmail.com>
2023-08-14 20:26:36 +0600

    samesite none

--- controllers/user.js
+++ controllers/user.js
@@ -17,7 +17,7 @@ export const auth_user = asyncHandler(async (req, res) => {
     res.cookie("jwt", user.getSignedJwtToken(), {
       httpOnly: true,
       secure: true,
-      sameSite: "lax",
+      sameSite: "none",
       maxAge: 30 * 24 * 24 * 60 * 60,
     });
     
