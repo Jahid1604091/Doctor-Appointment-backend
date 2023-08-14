6a545c369efac2c195d755bbb1fefe4370b93358
jahid1604091 <jh409780@gmail.com>
2023-08-14 16:06:35 +0600

    sdfssdfddfsddfjfgdfs

--- server.js
+++ server.js
@@ -24,7 +24,8 @@ const app = express();
 app.use(cors({
     origin: ['http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization','jwt'],
+    credentials:true
   }));
 // app.use(cors({
 //     credentials: true,
