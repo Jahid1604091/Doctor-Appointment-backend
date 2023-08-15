03aa1071354eca0ab0b869bd9826834bc7517697
jahid1604091 <jh409780@gmail.com>
2023-08-15 10:55:42 +0600

    change in register

--- controllers/user.js
+++ controllers/user.js
@@ -136,8 +136,8 @@ export const register = async (req, res, next) => {
     if (user) {
       res.cookie("jwt", user.getSignedJwtToken(), {
         httpOnly: true,
-        secure: process.env.NODE_ENV !== "development",
-        sameSite: "strict",
+        secure: true,
+        sameSite: "None",
         maxAge: 30 * 24 * 24 * 60 * 60,
       });
       return res.status(201).json({
