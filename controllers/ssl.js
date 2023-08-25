136d9bf27907b9d8b588f0939a282767d3542319
jahid1604091 <jh409780@gmail.com>
2023-08-25 10:47:32 +0600

    payment in https or http fix

--- controllers/ssl.js
+++ controllers/ssl.js
@@ -5,12 +5,15 @@ import { Appointment } from '../models/Appointment.js';
 export const initiateSSL = async (req, res) => {
     // console.log(`${req.protocol}://${req.get('host')}`)
     //using validate instead success url 
+
+    //appending https in production
+    const seucredOrNot = process.env.NODE_ENV === 'production' ? 's' :''
     let data = {
         ...req.body,
         currency: 'BDT',
-        success_url: `${req.protocol}s://${req.get('host')}/api/payment/ssl-validate`,
-        fail_url: `${req.protocol}s://${req.get('host')}/api/payment/ssl-fail`,
-        cancel_url: `${req.protocol}s://${req.get('host')}/api/payment/ssl-cancel`,
+        success_url: `${req.protocol}${seucredOrNot}://${req.get('host')}/api/payment/ssl-validate`,
+        fail_url: `${req.protocol}${seucredOrNot}://${req.get('host')}/api/payment/ssl-fail`,
+        cancel_url: `${req.protocol}${seucredOrNot}://${req.get('host')}/api/payment/ssl-cancel`,
         ipn_url: `${req.protocol}s://${req.get('host')}/api/payment/ssl-ipn`,
         shipping_method: 'Courier',
         product_name: 'Computer.',
@@ -40,7 +43,7 @@ export const initiateSSL = async (req, res) => {
         value_d: 'ref004_D'
     };
 
-    const sslcommer = new SSLCommerzPayment(process.env.STORE_ID, process.env.STORE_PASS, false) //true for live default false for sandbox
+    const sslcommer = new SSLCommerzPayment(process.env.STORE_ID, process.env.STORE_PASS, process.env.NODE_ENV==='production' ? true :false) //true for live default false for sandbox
     const r1 = await sslcommer.init(data);
     return res.status(200).json({
         success: true,
@@ -80,49 +83,6 @@ export const SSLvalidate = async (req, res) => {
     //tran_id = order_id
     const sslcz = new SSLCommerzPayment(process.env.STORE_ID, process.env.STORE_PASS, false)
     const r1 = await sslcz.validate({ val_id: req.val_id })
-  
-
-    //     status: 'VALID',
-    //   tran_date: '2023-07-10 01:22:10',
-    //   tran_id: '64a7970226a85c67bd52c960',
-    //   val_id: '230710122150Ori2BHMNv6o4Fw',
-    //   amount: '900.00',
-    //   store_amount: '877.5',
-    //   currency: 'BDT',
-    //   bank_tran_id: '230710122150KUSiTynDJumN4d',
-    //   card_type: 'BKASH-BKash',
-    //   card_no: '',
-    //   card_issuer: 'BKash Mobile Banking',
-    //   card_brand: 'MOBILEBANKING',
-    //   card_category: 'MOBILE',
-    //   card_sub_brand: '',
-    //   card_issuer_country: 'Bangladesh',
-    //   card_issuer_country_code: 'BD',
-    //   currency_type: 'BDT',
-    //   currency_amount: '900.00',
-    //   currency_rate: '1.0000',
-    //   base_fair: '0.00',
-    //   value_a: 'ref001_A',
-    //   value_b: 'ref002_B',
-    //   value_c: 'ref003_C',
-    //   value_d: 'ref004_D',
-    //   emi_instalment: '0',
-    //   emi_amount: '0.00',
-    //   emi_description: '',
-    //   emi_issuer: 'BKash Mobile Banking',
-    //   account_details: '',
-    //   risk_title: 'Safe',
-    //   risk_level: '0',
-    //   discount_percentage: '0',
-    //   discount_amount: '0.00',
-    //   discount_remarks: '',
-    //   APIConnect: 'DONE',
-    //   validated_on: '2023-07-10 01:22:18',
-    //   gw_version: '',
-    //   offer_avail: 1,
-    //   card_ref_id: 'dc1da4f52669828139e81ef5eb0f48a5a99ea054a131e00a562887d455417dd901',
-    //   isTokeizeSuccess: 0,
-    //   campaign_code: ''
 
     const appointment = await Appointment.findById(r1.tran_id);
     appointment.isPaid = true;
